751d006ad3cb512f747d136f65a2f960b049f0a3
habibcodes <habib.codes@gmail.com>
2021-11-08 23:39:16 -0500

    complete ex1-5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(womens_apparel: false)
+@mens_stores.each do |store|
+  puts "Men's Only Store --------"
+  puts "Store Name: #{store.name}."
+  puts "Annual Revenue: #{store.annual_revenue}."
+end
+
+@womens_stores = Store.where(mens_apparel: false)
+
+@womens_stores.each do |store|
+  if store.annual_revenue < 1000000
+    puts "Women's Only Store with < $1m Annual Revenue -----"
+    puts "Store Name: #{store.name}."
+    puts "Annual Revenue: #{store.annual_revenue}."
+  end
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "The total annual revenue for the entire company is $#{@total_revenue}. "
+
+@average_annual_revenue = Store.average(:annual_revenue)
+puts "The average annual revenue for teh company is $#{@average_annual_revenue}."
+
+@over_million_sales_stores = Store.where("annual_revenue > ?", 1000000).count
+puts "Total stores making over $1m are #{@over_million_sales_stores}."
+
